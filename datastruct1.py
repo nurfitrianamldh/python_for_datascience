18986f22cd9b2399e77c4ef0bea77dde36a742f2
nurfitrianamldh <nurfitrianamldh@gmail.com>
2024-10-29 19:06:45 +0700

    Add files via upload

---
+++ datastruct1.py
@@ -0,0 +1,9 @@
+#List other method
+angka = [38,100,45,20,70,7,91]
+print("Sebelum sort:",angka)
+
+angka.sort()
+print("Setelah sort:", angka)
+
+# alphabet_sort = sorted(sayuran)
+# print("Setelah sort:", alphabet_sort)
